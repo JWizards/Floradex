62cd37480ed564a20f2a3ca8227dabc24cc62e5a
JWizards <notirfanbasheer@gmail.com>
2022-11-06 10:19:06 +0100

    tuff

--- Assets/Scripts/displayPlant.cs
+++ Assets/Scripts/displayPlant.cs
@@ -10,15 +10,15 @@ public class displayPlant : MonoBehaviour
     public int pos;
 
     public TMP_Text name;
-    Image pfp;
+    public Image pfp;
 
     void Start(){
         GameDataManager manager = transform.parent.gameObject.GetComponent<GameDataManager>();
-        pfp = gameObject.GetComponent<Image>();
+        manager.readFile();
+        string thingy = manager.gameData.names[0];
+        Debug.Log(thingy);
         name.text = manager.gameData.names[pos];
-        Debug.Log(pfp.sprite);
-        string image_source = "Floramon" + manager.gameData.types[pos] + "_" + Math.Floor((manager.gameData.xp[pos])/100d);
-        Sprite creature =  Resources.Load <Sprite>(image_source);  
-        pfp.sprite = creature;
+        string image_source = "Floramon/" + manager.gameData.types[pos] + "_" + Math.Floor((manager.gameData.xp[pos])/100d);
+        pfp.sprite = Resources.Load <Sprite>(image_source);
     }
 }
