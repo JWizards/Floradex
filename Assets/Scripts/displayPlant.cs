63ee7286bf6b1f10d96a6f2a147499b2dc404b84
JWizards <notirfanbasheer@gmail.com>
2022-11-06 11:47:54 +0100

    stuff

--- Assets/Scripts/displayPlant.cs
+++ Assets/Scripts/displayPlant.cs
@@ -18,7 +18,7 @@ public class displayPlant : MonoBehaviour
         string thingy = manager.gameData.names[0];
         Debug.Log(thingy);
         name.text = manager.gameData.names[pos];
-        string image_source = "Floramon/" + manager.gameData.types[pos] + "_" + Math.Floor((manager.gameData.xp[pos])/100d);
+        string image_source = "Floramon/" + manager.gameData.types[pos] + "_" + Math.Min(Math.Floor((manager.gameData.xp[pos])/100d), 2);
         pfp.sprite = Resources.Load <Sprite>(image_source);
     }
 }
